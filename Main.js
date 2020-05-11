eefa1b69ab165174b9b80cd2243b5d95307e91ec
Logan-Rose <logan.rose0329@gmail.com>
2020-05-11 00:07:05 -0400

    Added functionality to drag and drop nodes that have already been placed, as well as recreate path to reflect new node placement

--- Main.js
+++ Main.js
@@ -1,13 +1,12 @@
 let canvas = document.querySelector('canvas')
 let context = canvas.getContext('2d')
-let WIDTH = 900
-let HEIGHT = 800
+let WIDTH = 760
+let HEIGHT = 500
+let RADIUS = 5
 let mouseDown = false
 
 canvas.width = WIDTH
 canvas.height = HEIGHT
-
-let button = document.getElementById("solve")
 
 //Objects
 
@@ -39,27 +38,33 @@ class Line{
         context.moveTo(this.startX, this.startY);
         context.lineTo(this.endX, this.endY);
         context.strokeStyle = "black";
-        context.lineWidth = 5;
+        context.lineWidth = 3;
         context.stroke();
     }
 }
 let lines = []
 
 //Event Listeners
-
-button.addEventListener('click',
+let solveButton = document.getElementById("solve")
+solveButton.addEventListener('click',
     function(){
+        lines = []
         for(let i =0; i < circles.length -1; i++){
-            lines[i] = new Line(circles[i].x, circles[i].y, circles[i+1].x, circles[i+1].y)
+            lines.push(new Line(circles[i].x, circles[i].y, circles[i+1].x, circles[i+1].y))
         }
-        console.log(lines)
     }
 )
+
+let nearest = -1
 
 window.addEventListener('mousemove',
     function(event){
         if(mouseDown === true && event.clientX < WIDTH && event.clientY < HEIGHT){
-            circles[circles.length-1] = new Circle(event.clientX, event.clientY, 10)
+            if(nearest === -1){
+                circles[circles.length-1] = new Circle(event.clientX, event.clientY, RADIUS)
+            } else{
+                circles[nearest] = new Circle(event.clientX, event.clientY, RADIUS)
+            }
         }
     } 
 )
@@ -67,8 +72,13 @@ window.addEventListener('mousedown',
     function(){
         mouseDown = true
         if(event.clientX < WIDTH && event.clientY < HEIGHT){
-            console.log("f")
-            circles[circles.length] = new Circle(event.clientX, event.clientY, 10)
+            nearest = near(circles, event.clientX, event.clientY)
+            console.log(nearest)
+            if(nearest === -1){
+                circles[circles.length] = new Circle(event.clientX, event.clientY, RADIUS)
+            } else{
+                circles[nearest] = new Circle(event.clientX, event.clientY, RADIUS)
+            }
         }
     } 
 )
@@ -77,6 +87,31 @@ window.addEventListener('mouseup',
         mouseDown = false
     } 
 )
+
+function near(nodes, mouseX, mouseY){
+    if(nodes.length === 0){
+        return -1
+    }
+    let closestX = Math.abs(nodes[0].x - mouseX)
+    let closestY = Math.abs(nodes[0].y - mouseY)
+    let closest =0
+    for(let i=0; i < nodes.length; i++){
+        if( (Math.abs(nodes[i].x - mouseX) < closestX ) && (Math.abs(nodes[i].y - mouseY) < closestY ) ){
+            closestX = Math.abs(nodes[i].x - mouseX)
+            closestY = Math.abs(nodes[i].y - mouseY)
+            closest = i
+        }
+    }
+    console.log(closestX + "," + closestY)
+    if(closestX < 10 && closestY < 10){
+        return closest
+    } else{
+        return -1
+    }
+    
+}
+
+
 
 //Drawing Function
 
