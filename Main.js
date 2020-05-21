8fc2aa4331f2736755accd19d1df5e36696d4e7e
Logan-Rose <logan.rose0329@gmail.com>
2020-05-20 23:44:25 -0400

    updated code style to better fir best practices

--- Main.js
+++ Main.js
@@ -1,31 +1,31 @@
-let canvas = document.querySelector('canvas')
-let context = canvas.getContext('2d')
+let canvas = document.querySelector('canvas');
+let context = canvas.getContext('2d');
 let styles = getComputedStyle(canvas),
 w = parseInt(styles.getPropertyValue("width"), 10),
 h = parseInt(styles.getPropertyValue("height"), 10);
-let WIDTH = w
-let HEIGHT = h
-let RADIUS = 7
-let mouseDown = false
+let WIDTH = w;
+let HEIGHT = h;
+let RADIUS = 7;
+let mouseDown = false;
 
-canvas.width = WIDTH
-canvas.height = HEIGHT
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
 
 //Objects
 
 class Circle{
     constructor(x, y, rad){
-        this.x = x
-        this.y = y
-        this.rad = rad
+        this.x = x;
+        this.y = y;
+        this.rad = rad;
     }
     draw(){
-        context.beginPath()
-        context.arc(this.x,this.y, this.rad,0,Math.PI*2, false)
-        context.strokeStyle = 'red'
-        context.stroke()
-        context.fill()
-        context.fillStyle='red'
+        context.beginPath();
+        context.arc(this.x,this.y, this.rad,0,Math.PI*2, false);
+        context.strokeStyle = 'red';
+        context.stroke();
+        context.fill();
+        context.fillStyle='red';
         context.font = "16px Arial";
     }
     getX(){ return this.x }
@@ -36,9 +36,9 @@ let circles = []
 
 class Line{
     constructor(start, end){
-        this.start = start
-        this.end = end
-        this.length = Math.round(Math.sqrt(Math.pow((this.start.getX() - this.end.getX()), 2) + Math.pow((this.start.getY() - this.end.getY()), 2)))
+        this.start = start;
+        this.end = end;
+        this.length = Math.round(Math.sqrt(Math.pow((this.start.getX() - this.end.getX()), 2) + Math.pow((this.start.getY() - this.end.getY()), 2)));
     }
     draw(val){
         context.beginPath();
@@ -60,70 +60,70 @@ class Line{
 
 }
 
-let lines = []
+let lines = [];
 
-let nearest = -1
+let nearest = -1;
 
 //Event Listeners
 
 //Button listners
-let solveButton = document.getElementById("solve")
+let solveButton = document.getElementById("solve");
 solveButton.addEventListener('click',
     function(){
         if(draw ===false){
-            draw = true
+            draw = true;
         } else{
-            draw =false
+            draw =false;
         }
     }
 )
 
-let cellNumbers = 10
-document.getElementById("generatedCells").innerHTML =  cellNumbers
-let generateCells = document.getElementById("generate")
+let cellNumbers = 10;
+document.getElementById("generatedCells").innerHTML =  cellNumbers;
+let generateCells = document.getElementById("generate");
 generateCells.addEventListener('click',
     function(){
-        circles = []
+        circles = [];
         for(let i=0; i < cellNumbers; i++){
-            circles[i] = new Circle(Math.floor(Math.random() * Math.floor(WIDTH)), Math.floor(Math.random() * Math.floor(HEIGHT)), RADIUS)
+            circles[i] = new Circle(Math.floor(Math.random() * Math.floor(WIDTH)), Math.floor(Math.random() * Math.floor(HEIGHT)), RADIUS);
         }
-        draw = true
+        draw = true;
     }
 )
 
-let slider = document.getElementById("slider")
+let slider = document.getElementById("slider");
 slider.addEventListener('mousedown', 
     function(){
-        mouseDown = true
-        document.getElementById("generatedCells").innerHTML = slider.value
-        cellNumbers = slider.value
+        mouseDown = true;
+        document.getElementById("generatedCells").innerHTML = slider.value;
+        cellNumbers = slider.value;
     }
 )
 slider.addEventListener('mouseup', 
     function(){
-        mouseDown = false
+        mouseDown = false;
     }
 )
 slider.addEventListener('mousemove', 
     function(){
         if(mouseDown === true){
-            document.getElementById("generatedCells").innerHTML = slider.value
-            cellNumbers = slider.value
+            document.getElementById("generatedCells").innerHTML = slider.value;
+            cellNumbers = slider.value;
         }
     }
 )
 
 //Map listeners
-let draw = false
+let draw = false;
 
 window.addEventListener('mousemove',
     function(event){
         var rect = canvas.getBoundingClientRect();
         if(mouseDown === true && event.clientX < WIDTH && event.clientY- rect.top < HEIGHT){
             if(nearest === -1){
-                circles[circles.length-1] = new Circle(event.clientX, event.clientY- rect.top, RADIUS)
+                circles[circles.length-1] = new Circle(event.clientX, event.clientY- rect.top, RADIUS);
             } else{
-                circles[nearest] = new Circle(event.clientX, event.clientY- rect.top, RADIUS)
+                circles[nearest] = new Circle(event.clientX, event.clientY- rect.top, RADIUS);
             }
         }
     } 
@@ -133,11 +133,11 @@ window.addEventListener('mousedown',
         var rect = canvas.getBoundingClientRect();
         mouseDown = true
         if(event.clientX < WIDTH && event.clientY - rect.top < HEIGHT){
-            nearest = near(circles, event.clientX, event.clientY- rect.top)
+            nearest = near(circles, event.clientX, event.clientY- rect.top);
             if(nearest === -1){
-                circles[circles.length] = new Circle(event.clientX, event.clientY- rect.top , RADIUS)
+                circles[circles.length] = new Circle(event.clientX, event.clientY- rect.top , RADIUS);
             } else{
-                circles[nearest] = new Circle(event.clientX, event.clientY- rect.top , RADIUS)
+                circles[nearest] = new Circle(event.clientX, event.clientY- rect.top , RADIUS);
             }
         }
     } 
@@ -147,7 +147,7 @@ window.addEventListener('dblclick',
     function(){
         var rect = canvas.getBoundingClientRect();
         if(event.clientX < WIDTH && (event.clientY - rect.top) < HEIGHT){
-            nearest = near(circles, event.clientX, (event.clientY - rect.top))
+            nearest = near(circles, event.clientX, (event.clientY - rect.top));
             if(nearest != -1){
                 circles.splice(nearest, 1);
             }
@@ -157,7 +157,7 @@ window.addEventListener('dblclick',
 
 window.addEventListener('mouseup',
     function(){
-        mouseDown = false
+        mouseDown = false;
     } 
 )
 
@@ -165,210 +165,173 @@ window.addEventListener('mouseup',
 //Algorithms
 
 function orderedTraversal(){
-    lines = []
+    lines = [];
     for(let i =0; i < circles.length -1; i++){
-        lines.push(new Line(circles[i],circles[i+1]))
+        lines.push(new Line(circles[i],circles[i+1]));
     }
-    return lines
+    return lines;
 }
 
 function greedy(){
-    lines = []
-    let visited = []
-    let shortest = -1
-    let next = -1
-    let i =0
+    lines = [];
+    let visited = [];
+    let shortest = -1;
+    let next = -1;
+    let i =0;
     while(visited.length < circles.length){
-        shortest = -1
+        shortest = -1;
         for(let j =1; j < circles.length; j++){
             if(  (distance(circles[i], circles[j]) < shortest || shortest === -1) && i != j && !visited.includes(circles[j]) ){
-                shortest = distance(circles[i], circles[j])
-                next = j
+                shortest = distance(circles[i], circles[j]);
+                next = j;
             }
         }
-        visited.push(circles[next])
-        lines.push(new Line(circles[i], circles[next]))
-        i = next
+        visited.push(circles[next]);
+        lines.push(new Line(circles[i], circles[next]));
+        i = next;
     }
-    console.log(complete(lines))
-    return lines
+    console.log(complete(lines));
+    return lines;
 }
 
 function pairwiseExchange(){
-    lines = []
-    let visited = []
-    let shortest = -1
-    let next = -1
-    let i =0
-    while(visited.length < circles.length){
-        shortest = -1
-        for(let j =1; j < circles.length; j++){
-            if(  (distance(circles[i], circles[j]) < shortest || shortest === -1) && i != j && !visited.includes(circles[j]) ){
-                shortest = distance(circles[i], circles[j])
-                next = j
-            }
-        }
-        visited.push(circles[next])
-        lines.push(new Line(circles[i], circles[next]))
-        i = next
-    }
-    let swapped = []
-    for(let i =0; i < lines.length; i++){
-        swapped[i] = lines[i]
-    }
-    
-    for(let i=0; i < lines.length; i ++){
-        for(let j=0; j < lines.length; j++){
-            swapped = swap(lines, i, j)
-            if(totalLengthLines(swapped) < totalLengthLines(lines) && complete(swappedLines)){
-                lines = swapped
-            }
-        }
-    }d
-    console.log("===" + complete(lines))
-    return lines
 
 }
 
 function bruteForce(){
-    let lines = []
-    console.log("frig off")
-    let paths = [[circles[0]]]
-    let prevLength =0 
+    let lines = [];
+    console.log("frig off");
+    let paths = [[circles[0]]];
+    let prevLength = 0 ;
     for(let j =0; j < paths.length; j++){
         for(let i =0; i < circles.length; i++){
             if(!paths[j].includes(circles[i])){
-                paths.push([...paths[j], circles[i]])
+                paths.push([...paths[j], circles[i]]);
             }
         }
     }
-    paths = paths.filter(x => x.length === circles.length)
-    let shortest = paths[0]
+    paths = paths.filter(x => x.length === circles.length);
+    let shortest = paths[0];
     for( let i =0; i < paths.length; i++){
-        console.log("====")
-        console.log(totalLengthPoints(paths[i]))
+        console.log("====");
+        console.log(totalLengthPoints(paths[i]));
         if(totalLengthPoints(paths[i]) < totalLengthPoints(shortest)){
-            shortest = paths[i]
+            shortest = paths[i];
         }
     }
-
     for(let i =0; i< paths.length;i++){
-        console.log(paths[i])
-        console.log(totalLengthPoints(paths[i]))
+        console.log(paths[i]);
+        console.log(totalLengthPoints(paths[i]));
     }
     for(let i =0; i < shortest.length -1; i++){
-        lines.push(new Line(shortest[i],shortest[i+1]))
+        lines.push(new Line(shortest[i],shortest[i+1]));
     }
-    
-    return lines
+    return lines;
 
 }
 
 
-let algorithms = [orderedTraversal, bruteForce, greedy, pairwiseExchange]
+let algorithms = [orderedTraversal, bruteForce, greedy, pairwiseExchange];
 
 //Helper functions
 function near(nodes, mouseX, mouseY){
     if(nodes.length === 0){
-        return -1
+        return -1;
     }
-    let closestX = Math.abs(nodes[0].x - mouseX)
-    let closestY = Math.abs(nodes[0].y - mouseY)
-    let closest =0
+    let closestX = Math.abs(nodes[0].x - mouseX);
+    let closestY = Math.abs(nodes[0].y - mouseY);
+    let closest =0;
     for(let i=0; i < nodes.length; i++){
         if( (Math.abs(nodes[i].getX() - mouseX) < closestX ) && (Math.abs(nodes[i].getY() - mouseY) < closestY ) ){
-            closestX = Math.abs(nodes[i].getX() - mouseX)
-            closestY = Math.abs(nodes[i].getY() - mouseY)
-            closest = i
+            closestX = Math.abs(nodes[i].getX() - mouseX);
+            closestY = Math.abs(nodes[i].getY() - mouseY);
+            closest = i;
         }
     }
     console.log(closestX + "," + closestY)
     if(closestX < 20 && closestY < 20){
-        return closest
+        return closest;
     } else{
-        return -1
+        return -1;
     }
     
 }
 
 function swap(passedLines, p1, p2){
-    let list = []
+    let list = [];
     for(let i=0; i < passedLines.length; i++){
-        list[i] = passedLines[i]
+        list[i] = passedLines[i];
     }
-    let temp= list[p1].end
-    list[p1].end = list[p2].end
-    list[p2].end = temp
-
-    list[p1].length = Math.round(Math.sqrt(Math.pow((list[p1].start.getX() - list[p1].end.getX()), 2) + Math.pow((list[p1].start.getY() - list[p1].end.getY()), 2)))
-
-    list[p2].length = Math.round(Math.sqrt(Math.pow((list[p2].start.getX() - list[p2].end.getX()), 2) + Math.pow((list[p2].start.getY() - list[p2].end.getY()), 2)))
-
-    return list
+    let temp= list[p1].end;
+    list[p1].end = list[p2].end;
+    list[p2].end = temp;
+    list[p1].length = Math.round(Math.sqrt(Math.pow((list[p1].start.getX() - list[p1].end.getX()), 2) + Math.pow((list[p1].start.getY() - list[p1].end.getY()), 2)));
+    list[p2].length = Math.round(Math.sqrt(Math.pow((list[p2].start.getX() - list[p2].end.getX()), 2) + Math.pow((list[p2].start.getY() - list[p2].end.getY()), 2)));
+    return list;
 }
 function distance(p1, p2){
-    return Math.sqrt(Math.pow((p1.getX() - p2.getX()), 2) + Math.pow((p1.getY() - p2.getY()), 2))
+    return Math.sqrt(Math.pow((p1.getX() - p2.getX()), 2) + Math.pow((p1.getY() - p2.getY()), 2));
 }
 
 function totalLengthLines(list){
-    let total = 0
+    let total = 0;
     for(let i=0; i < list.length; i++){
-        total = total + list[i].length
+        total = total + list[i].length;
     }
-    return total
+    return total;
 }
 
 function totalLengthPoints(list){
-    let total = 0
-    console.log(list[0])
+    let total = 0;
+    console.log(list[0]);
     for(let i=0; i < list.length -1; i++){
-        total = total + Math.round(Math.sqrt(Math.pow((list[i].getX() - list[i+1].getX()), 2) + Math.pow((list[i].getY() - list[i+1].getY()), 2)))
+        total = total + Math.round(Math.sqrt(Math.pow((list[i].getX() - list[i+1].getX()), 2) + Math.pow((list[i].getY() - list[i+1].getY()), 2)));
     }
-    return total
+    return total;
 }
 
 
 function complete(list){
-    let startPoint = list[0].getStart()
-    let endPoint = list[0].getStart()
-    let count =0
+    let startPoint = list[0].getStart();
+    let endPoint = list[0].getStart();
+    let count =0;
     for(let i =0; i < list.length; i++){
-        startPoint = list[i].getStart()
+        startPoint = list[i].getStart();
 
         if(startPoint.getX() !== endPoint.getX()){
-            return false
+            return false;
         }
-        endPoint = list[i].getEnd()
-        count = i
+        endPoint = list[i].getEnd();
+        count = i;
     }
 
-    return true
+    return true;
 }
 
 //Drawing Function
 
 function animate(){
-    requestAnimationFrame(animate)
+    requestAnimationFrame(animate);
     document.getElementById("totalLength").innerHTML = totalLengthLines(lines);
-    context.clearRect(0,0, WIDTH, HEIGHT)
+    context.clearRect(0,0, WIDTH, HEIGHT);
         
     for(let i=0; i < circles.length; i++){
-        circles[i].draw(i)
+        circles[i].draw(i);
     }
     if(draw === true){
-        let pos = -1
-        let algorithmOptions = document.getElementsByName('algorithmOptions')
+        let pos = -1;
+        let algorithmOptions = document.getElementsByName('algorithmOptions');
         for(let i =0; i < algorithmOptions.length; i++){
             if(algorithmOptions[i].checked){
-                pos = i
+                pos = i;
             }
         }
-        lines = algorithms[pos]()
+        lines = algorithms[pos]();
         for(let i=0; i < lines.length; i++){
-            lines[i].draw(i)   
+            lines[i].draw(i);
         }
     }
 }
 
 
-animate()
+animate();
